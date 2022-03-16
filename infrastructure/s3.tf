509c02a3994caf8e979c904e798a6b3d752bfc2f
JoaoPauloSarzedasRibeiro <sarzedas.joaopaulo@gmail.com>
2022-03-16 08:17:03 -0300

    add backend tf s3

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -21,18 +21,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "datalake" {
       }
     }
 }
-
-# 2 - Ingestão de um arquivo no bucket criado anteriormente utilizando codigo
-resource "aws_s3_bucket_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark_from_tf.py"
-  acl    = "private"
-  source = "../job_spark_enem2020.py"
-  etag   = filemd5("../job_spark_enem2020.py")
-}
-
-#Definição do provedor conforme especificado na variavel region
-provider "aws" {
-  region = var.region
-}
-
